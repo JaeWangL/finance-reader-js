1fa53b9a753e0a9f41a098fffbe7c9dd278f71f9
JaeWangL <jnso5072@outlook.kr>
2021-04-27 19:04:05 +0900

    [Change] Rename 'itemCode' to 'symbolCode'

--- libs/index.ts
+++ libs/index.ts
@@ -2,13 +2,13 @@ import { MarketType } from './enums';
 import { readDailyDataAsync } from './naver';
 
 export const readDataAsync = async (
-  itemCode: string,
+  symbolCode: string,
   startDate: Date = new Date(1970, 1, 1),
   endDate: Date = new Date(),
   market: MarketType = MarketType.KRX,
 ): Promise<string> => {
   try {
-    const res = await readDailyDataAsync(itemCode);
+    const res = await readDailyDataAsync(symbolCode);
 
     return JSON.stringify(res);
   } catch (e) {
